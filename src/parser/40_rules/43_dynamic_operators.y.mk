452578051ac0d0bf1e1cb7f667aedc5c0afb5e4e
vslutov <vslutov@yandex.ru>
2014-07-23 17:53:26 +0400

    remove operators

--- src/parser/40_rules/43_dynamic_operators.y.mk
+++
@@ -1,5 +0,0 @@
-$(PARSER_BUILD_DIR)/40_rules/43_dynamic_operators.y : $(PARSER_SOURCE_DIR)/40_rules/43_dynamic_operators.y.c
-	mkdir -p $(PARSER_BUILD_DIR)/40_rules
-	tcc -run $(PARSER_SOURCE_DIR)/40_rules/43_dynamic_operators.y.c > $(PARSER_BUILD_DIR)/40_rules/43_dynamic_operators.y
-
-PARSER_BUILD_FILES := $(PARSER_BUILD_FILES) $(PARSER_BUILD_DIR)/40_rules/43_dynamic_operators.y
